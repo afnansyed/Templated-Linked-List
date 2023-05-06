8d0bee1c11c90400d591acddb2c07fc6e2326479
Afnan Syed <44105687+afnansyed@users.noreply.github.com>
2023-05-06 16:50:16 -0400

    Add files via upload

---
+++ Project1_part3/cmake-build-debug/CMakeFiles/Project1_part3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project1_part3.dir/main.cpp.obj"
+  "Project1_part3.exe"
+  "Project1_part3.exe.manifest"
+  "Project1_part3.pdb"
+  "libProject1_part3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project1_part3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
